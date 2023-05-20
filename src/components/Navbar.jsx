bd17d4a23039e301ca2b94c6de5dc18a4635142f
ShufanSun <117487942+ShufanSun@users.noreply.github.com>
2023-05-20 09:43:37 -0500

    modified the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { styles } from '../styles';
 import { navLinks } from '../constants';
-import { logo, menu, close } from '../assets';
+import { logo, menu, close} from '../assets';
 
 const Navbar = () => {
     const [active, setActive] = useState('');
@@ -13,9 +13,14 @@ const Navbar = () => {
                     onclick={() => {
                         setActive("");
                         window.scrollTo(0, 0);
-                    }}                >
-                    <img src={logo} alt="logo" className="w-9 h-9 object-contain"/>
+                    }}
+                >
+                    <img src={logo} alt="logo" className="w-9 h-9 object-contain" />     
+                    <p className="text-white text-[18px] font-bold
+cursor-pointer">Sofine <span className="sm:block hidden">Sun</span></p>
+
                 </Link>
+                <p className="text-red-500">asdsa</p>
             </div>
         </nav>
   )
