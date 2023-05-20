e0eec1de513b5aeb328a68d5fe67b643989e919a
ShufanSun <117487942+ShufanSun@users.noreply.github.com>
2023-05-20 04:39:19 -0500

    fixed the error

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { styles } from '../styles';
-import { navlinks } from '../constants';
+import { navLinks } from '../constants';
 import { logo, menu, close } from '../assets';
 
 const Navbar = () => {
-    const [active, setActive]=useState('')
+    const [active, setActive] = useState('');
     return (
         <nav className={ `${styles.paddindX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
             <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
