2cd54d4820c6515aee26838d96b2fb2242985140
ShufanSun <117487942+ShufanSun@users.noreply.github.com>
2023-05-21 08:53:16 -0500

    added more models and changed the theme

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,7 +18,7 @@ const Navbar = () => {
                 >
                     <img src={logo} alt="logo" className="w-9 h-9 object-contain" />     
                     <p className="text-white text-[18px] font-bold
-cursor-pointer flex">Sofine &nbsp;<span className="sm:block hidden">Sun</span></p>
+cursor-pointer flex">Shufan &nbsp;<span className="sm:block hidden">Sun</span></p>
 
                 </Link>
                 <ul className="list-none hidden sm:flex flex-row gap-10">
