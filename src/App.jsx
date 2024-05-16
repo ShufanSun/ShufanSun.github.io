cfc6c73a70ec99d2f3ba872793281ef1e66ff847
ShufanSun <ssun329@wisc.edu>
2024-05-15 21:32:48 -0500

    changed to hashrouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works,Gallery ,StarsCanvas } from './components';
 import ParticlesBackground from './components/ParticlesBackground';
 
